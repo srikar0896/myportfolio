e3fd80fe1b4d1fb44edf26e74e32e26442e348b4
srikar0896 <srikar.0896@gmail.com>
2025-11-10 23:43:36 -0800

    add analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,25 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+import React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtag-script"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-FR1PN1LFSD"
+    />,
+    <script
+      key="gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FR1PN1LFSD');
+        `,
+      }}
+    />,
+  ])
+}
